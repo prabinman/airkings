2650c5d14f2e38176dbd44664b070cc8a34c3b06
Prabin Shrestha <shrestha.prabinman@gmail.com>
2018-02-26 13:36:30 +0545

    changed NODE_ENV to PORT in server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const webpack = require('webpack');
 const app = express();
 const config = require('./webpack.config.js');
 
-const port = process.env.NODE_ENV || 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log('Server Started on port ' + port + '!\n');
